07fbe4fa85400ff606f7fd7063a5afa5edcb7a9d
Anirudh Swaminathan <aniswami97@gmail.com>
2016-08-11 22:31:43 +0530

    Deleted Interrnal README. Modified package.json

--- ChemWeb19Nitt/routes/register.js
+++ ChemWeb19Nitt/routes/register.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host  :  'localhost',
+  user  :  'master',
+  password  : 'Admin@123',
+  database  : 'chemstudents'
+});
 
 var allStuds;
 
 router.get('/',function(req,res,next){
-  var conn = req.app.locals.connection;
+  //var conn = req.app.locals.connection;
   conn.connect();
   conn.query("SELECT * FROM students as SOLUTION",function(err, rows, fields){
     if(err){
@@ -17,7 +24,7 @@ router.get('/',function(req,res,next){
     }
     allStuds = rows;
     res.render('register',{studs: allStuds});
+    conn.end();
   });
 });
-
 module.exports = router;
