140ba0f7de1c87d3bf2f30e5a44ab89edfaaceac
Anirudh Swaminathan <aniswami97@gmail.com>
2016-08-14 20:33:58 +0530

    Front side validation completed

--- ChemWeb19Nitt/public/javascripts/regval.js
+++ ChemWeb19Nitt/public/javascripts/regval.js
@@ -1,134 +1,139 @@
 function btnClick(){
-	return validate();
+  return validate();
 }
 
+// A function to validate input
 function validate(){
-	var n = document.getElementById("name").value;
-	var e = document.getElementById("email").value;
-  var r = document.getElementById("roll").value;
-	var p = document.getElementById("mobile").value;
-	var pass = document.getElementById("pass").value;
-	var c = document.getElementById("confp").value;
-  var nat = document.getElementById("place").value;
-  var dob = document.getElementById("dob").value;
+  var r = document.getElementById('roll').value;
+  var n = document.getElementById('name').value;
+  var e = document.getElementById('email').value;
+  var m = document.getElementById('mobile').value;
+  var dob = document.getElementById('dob').value;
+  var nat = document.getElementById('place').value;
+  var pa = document.getElementById('pass').value;
+  var con = document.getElementById('confp').value;
 
-  alert("In Validate()");
-  alert(n+"\n"+e+"\n"+r+"\n"+p+"\n"+pass+"\n"+c+"\n"+nat+"\n"+dob);
+  // Check roll number
+  // Only Chemical Students of 2nd year.
+  if(Math.floor(r/Math.pow(10,3))!=102115){
+    alert('Incorrect Roll Number for a Chemical 2nd year student');
+    return false;
+  }
 
-  // Check dob,place.
-  // TODO Perform form validation constraint in HTML itself.
-	//Test name
-	if (!(/[a-zA-Z]/.test(n))) {
-		alert('No Letter Found in name');
-		return false;
-	}
-	if(/\d/.test(n)){
-		alert('Name shouldn\'t contain any number');
-		return false;
-	}
-	if (!(/\S/.test(n))) {
-		alert('The name must not be only whiteSpace');
-		return false;
-	}
+  // Check name
+  if(!(n.match('[a-zA-Z][a-zA-Z ]+|[a-zA-Z]'))){
+    alert('The Name must not be empty and mustn\'t contain special characters');
+    return false;
+  }
 
-	//Check phone
-	if(!(/\d/.test(p))){
-		alert("Phone must contain only digits");
-		return false;
-	}
-	if(p.length !== 10){
-		alert("Mobile number must contain 10 digits");
-		return false;
-	}
-
-  // Check Roll number
-  if(!(Math.floor(r/Math.pow(10,6)) ===102)){
-		alert('Incorrect Roll Number');
-		return false;
-	}
-
-	//Check mail
-	if(e === ""){
-		alert("Email must not be empty");
-		return false;
-	}
-	if(!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.+[a-zA-Z]{2,4}$/.test(e))){
-		alert("Invalid mail address");
-		return false;
-	}
+  // Check WebMail
   if(e.length!=18){
-		alert('Enter valid email. Should be \'your roll number\'@nitt.edu');
-		return false;
-	}
-	else{
-		var ro = e.substring(0,9);
-		var en = e.substring(9,18);
-		//alert('ro is '+ro+'\nen is '+en);
-		if(!(ro === r)){
-			alert("Valid Email is'your roll number'@nitt.edu");
-			return false;
-		}
-		if(!(en === "@nitt.edu")){
-			alert("Valid Email is 'your roll number'@nitt.edu");
-			return false;
-		}
-	}
+    alert('Webmail must be <roll>@nitt.edu');
+    return false;
+  } else {
+    //alert('e is '+e+'\n r is '+r);
+    //alert('e start is '+e.substr(0,9)+'\n e end is '+e.substr(9,18));
+    if(e.substr(0,9)!=r || e.substr(9,18)!='@nitt.edu'){
+      alert('Webmail must be <roll>@nitt.edu');
+      return false;
+    }
+  }
 
-	//Check pass
-	if(!(/\S/.test(pass))){
-		alert("The password must contain atleast 1 non-whitespace character, and must not be empty");
-		return false;
-	}
-	if(!(/\S/.test(c))){
-		alert("The Confirm password must contain atleast 1 non-whitespace character, and must not be empty");
-		return false;
-	}
-	if(pass !== c){
-		alert("Please re-enter the exact same password in the Confirm Password field");
-		return false;
-	}
+  // Check Mobile
+  if(!(m.match('^[0-9]{10}$'))){
+    alert('Incorrect Mobile Number');
+    return false;
+  }
 
-  /*
-	//Check the pic
-	if( document.getElementById("imageUpload").files.length == 0 ){
-		alert("no files selected");
-		return false;
-	}
-  */
-	return true;
+  // Check DOB
+  if(dob!=""){
+    if(dob === "" || dob.length!=10){
+      alert('Incorrect dob. Must be yyyy/mm/dd');
+      return false;
+    }
+    var yea = parseInt(dob.substr(0,4));
+    var mon = parseInt(dob.substr(5,2));
+    var day = parseInt(dob.substr(8,2));
+    if(isNaN(day) || isNaN(mon) || isNaN(yea)){
+      alert('Incorrect date. Must be yyyy/mm/dd');
+      return false;
+    }
+    if(mon<0 || mon>13){
+      alert('Incorrect month. Must be yyyy/mm/dd');
+      return false;
+    }
+    if(day<0 || yea<0){
+      alert('Incorrect day or year. Must be yyyy/mm/dd');
+      return false;
+    }
+    if((mon==1||mon==3||mon==5||mon==7||mon==8||mon==10||mon==12) && day>31){
+      alert('Incorrect date. Must be <=31');
+      return false;
+    }
+    if((mon==4||mon==6||mon==9||mon==11) && day>30){
+      alert('Incorrect date. Must be <=30');
+      return false;
+    }
+    if(mon==2){
+      //alert('It\'s February!');
+      if(isLeap(yea)){
+        if(day>29){
+          alert('Incorrect date. Must be <=29');
+          return false;
+        }
+      } else {
+        //alert('Not a leap year');
+        if(day>28){
+          alert('Incorrect date. Must be <=28');
+          return false;
+        }
+      }
+    }
+  }
+
+  // Check place
+  if(nat!=""){
+    if(!(nat.match('[a-zA-Z][a-zA-Z ]+|[a-zA-Z]'))){
+      alert('The place mustn\'t contain special characters');
+      return false;
+    }
+  }
+
+  // Check pass and confp
+  if(pa == "" || con == ""){
+    alert('Password and confirm pasword must not be null');
+    return false;
+  }
+  var st = pa;
+  if (/\s/g.test(st)) {
+    // string only contained whitespace (ie. spaces, tabs or line breaks)
+    alert('Password mustn\'t contain any whitespace');
+    return false;
+  }
+  if(pa!=con){
+    alert('Please enter the same value of password in the confirm password field');
+    return false;
+  }
+
+  return true;
 }
 
-/*
-AJAX For form submission
-document.getElementById('regis').addEventListener("submit",function(e){
-
-	e.preventDefault();
-	var f = e.target;
-	//alert(f);
-	var data = new FormData(f);
-	//alert(data);
-	//alert("Method is "+f.method);
-	//alert("Action is "+f.action);
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			//alert(xhttp.responseText);
-			if(xhttp.responseText === "Success"){
-				document.getElementById('regis').reset();
-
-				alert('Successfully registered. Taking you to the login page now');
-				window.location.href = '/../Delta_2016_3/';
-            }
-            else{
-				if(xhttp.responseText === "Already logged in"){
-					alert('Please Logout before registering a new user');
-					location.reload();
-				}
-                else alert(xhttp.responseText);
-            }
-		}
-	};
-	xhttp.open(f.method,f.action,true);
-	xhttp.send(data);
-});
-*/
+// To check if the year is a leap year
+function isLeap(ye){
+  var ret = false;
+  //alert('In isLeap\nye is '+ye);
+  if(ye%4==0){
+    if(ye%100 == 0){
+      if(ye%400 == 0){
+        ret = true;
+      } else {
+        ret = false;
+      }
+    } else {
+      ret = true;
+    }
+  } else {
+    ret = false;
+  }
+  return ret;
+}
